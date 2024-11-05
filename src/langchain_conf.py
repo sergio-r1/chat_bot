92c59f307bd8e0aaa98639ebf371b3f8d9b6e071
sergio-r1 <sergio-r1@outlook.com>
2024-11-05 18:08:34 -0300

    feature: buffered data persistence

--- src/langchain_conf.py
+++ src/langchain_conf.py
@@ -1,5 +1,9 @@
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_groq import ChatGroq
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationChain
+
+memory = ConversationBufferMemory()
 
 chat = ChatGroq(
     temperature=0,
@@ -7,14 +11,27 @@ chat = ChatGroq(
 )
 
 prompt_template = ChatPromptTemplate.from_messages([
-    ("system", "Você é um assistente amigável."),
-    ("human", "{input}"),
+    ("system", "Você é um assistente amigável. Aqui está o contexto anterior: {history}"),
+    ("human", "{input}")
 ])
 
-chain = prompt_template | chat
+chain = ConversationChain(
+    llm=chat,
+    prompt=prompt_template,
+    memory=memory
+)
 
 def generate_response(user_input):
-    response = chain.invoke({"input": user_input})
-
-    response_content = response.content
-    return response_content
+    try:
+        response = chain.invoke({"input": user_input})
+        
+        if isinstance(response, dict) and "response" in response:
+            return response["response"]
+        elif isinstance(response, dict) and "content" in response:
+            return response["content"]
+        else:
+            return "Algo inesperado aconteceu. \nPeço desculpas pelo inconveniente."
+    except ValueError as ve:
+        raise ValueError(f"Erro de Valor: {str(ve)}")
+    except Exception as e:
+        raise RuntimeError(f"Ocorreu um erro ao gerar a resposta: {str(e)}")
--- src/main.py
+++ src/main.py
@@ -2,8 +2,7 @@ import streamlit as st
 from langchain_conf import generate_response
 
 st.title("Chatbot")
-user_input = st.text_input("Pergunte ao chatbot:")
+user_input = st.text_input("Faça a sua pergunta ao chatbot:")
 if user_input:
-    st.session_state.input_text = ""
     response = generate_response(user_input)
     st.write(response)
