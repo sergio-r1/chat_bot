d14d16f9d91e38c2615dac7f7087960a35db5537
sergio-r1 <sergio-r1@outlook.com>
2024-11-11 22:51:45 -0300

    refactor: create the main class

--- src/main.py
+++ src/main.py
@@ -1,8 +1,19 @@
 import streamlit as st
 from langchain_conf import generate_response
 
-st.title("Chatbot")
-user_input = st.text_input("Faça a sua pergunta ao chatbot:")
-if user_input:
-    response = generate_response(user_input)
-    st.write(response)
+
+class ChatbotMain():
+    def __init__(self, title):
+        self.title = title
+        
+    def run(self):
+        st.title(self.title)
+        user_input = st.text_input("Faça a sua pergunta ao chatbot:")
+        if user_input:
+            response = generate_response(user_input)
+            st.write(response)
+
+
+if __name__ == "__main__":
+    app = ChatbotMain("Chatbot interativo")
+    app.run()
